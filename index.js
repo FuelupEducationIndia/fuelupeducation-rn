94d64adda4fcd5fa2b5f67a299f683962d84684c
Muddaser-Nisar <muddasernisar786@gmail.com>
2021-03-29 13:41:32 +0500

    my work for one page is in src_muddaser folder

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ import { enableScreens } from 'react-native-screens'
 enableScreens()
 
 AppRegistry.registerComponent(appName, () => AppProvider)
+
+//Here is index changes to directly run my js file from src_muddaser folder
+
+// /**
+//  * @format
+//  */
+
+// import { AppRegistry } from 'react-native'
+// import App from './src_muddaser/App'
+// import { name as appName } from './app.json'
+
+// AppRegistry.registerComponent(appName, () => App)
