71af3a8306b44e827d761323f212972126d9bd54
amarpsp10 <amarpsp10@gmail.com>
2021-10-01 14:05:56 -0700

    teachers route created

---
+++ src/Teachers/TeacherScreens/HomeTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function HomeTab() {
+  return (
+    <View>
+      <Text>Hello world here i am</Text>
+    </View>
+  )
+}
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,6 +3,7 @@ import {
   CardStyleInterpolators,
   StackNavigationProp,
 } from '@react-navigation/stack'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 
 export interface AppNavigationProps<RouteName extends keyof AppRoutes> {
   navigation: StackNavigationProp<AppRoutes, RouteName>
@@ -11,11 +12,17 @@ export interface AppNavigationProps<RouteName extends keyof AppRoutes> {
 
 export type AppRoutes = {
   Authentication: undefined
+  Teacher: undefined
 }
 
 export interface AuthNavigationProps<RouteName extends keyof AuthRoutes> {
   navigation: StackNavigationProp<AuthRoutes, RouteName>
   route: RouteProp<AuthRoutes, RouteName>
+}
+
+export interface TeacherNavigationProps<RouteName extends keyof TeacherRoutes> {
+  navigation: BottomTabNavigationProp<TeacherRoutes, RouteName>
+  route: RouteProp<TeacherRoutes, RouteName>
 }
 
 export type AuthRoutes = {
@@ -26,6 +33,10 @@ export type AuthRoutes = {
   SignUp: undefined
   SignUpInfo: undefined
   SignUpSuccess: String
+}
+
+export type TeacherRoutes = {
+  Home: undefined
 }
 
 export const stackScreenOptions = {
