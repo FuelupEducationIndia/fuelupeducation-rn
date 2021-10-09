ce77ee71b29e3b18c5f363feab0c43a903d65004
amarpsp10 <amarpsp10@gmail.com>
2021-10-08 19:14:57 -0700

    browsecours screen filtermodal added

--- src/Teachers/TeacherHomeTopTabs/BrowserCourse.tsx
+++ src/Teachers/TeacherHomeTopTabs/BrowserCourse.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, ScrollView, StyleSheet, FlatList } from 'react-native'
+import { View, ScrollView, StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import theme, { Card, Text } from '../../theme'
 import Icon from 'react-native-vector-icons/Feather'
-import { SearchBar, CourseCard, Button } from '../../components'
+import { SearchBar, CourseCard, Button, FilterModal } from '../../components'
 
 // Testing Data
 import Thumbnail from '../../assets/images/thumbnail1.png'
@@ -51,6 +51,7 @@ const data = [
 
 const BrowserCourse = () => {
   const [search, setSearch] = React.useState('')
+  const [showFilter, setShowFilter] = React.useState(false)
 
   const onSearch = () => {
     console.log('searching')
@@ -62,20 +63,14 @@ const BrowserCourse = () => {
   const onNext = () => {
     return
   }
-  const renderItem = ({ item }: any) => (
-    <CourseCard
-      id={item.id}
-      title={item.title}
-      thumbnail={item.thumbnail}
-      author={item.author}
-    />
-  )
 
   return (
     <ScrollView>
       <Card
         variant='elevated'
         borderRadius='l'
+        borderTopLeftRadius='none'
+        borderTopRightRadius='none'
         marginBottom='l'
         backgroundColor='white'
         padding='s'>
@@ -85,19 +80,32 @@ const BrowserCourse = () => {
           onSubmit={() => onSearch()}
         />
         <View style={{ alignItems: 'flex-start' }}>
-          <TouchableOpacity activeOpacity={0.9} style={styles.filterButton}>
+          <TouchableOpacity
+            onPress={() => setShowFilter(true)}
+            activeOpacity={0.9}
+            style={styles.filterButton}>
             <Text variant='body' marginRight='s' color='primary'>
               Filter
             </Text>
             <Icon name={'filter'} color={theme.colors.primary} size={15} />
           </TouchableOpacity>
         </View>
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          numColumns={2}
+        <FilterModal
+          visible={showFilter}
+          onRequestClose={() => setShowFilter(false)}
         />
+        <View
+          style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+          {data.map((data) => {
+            return (
+              <CourseCard
+                title={data.title}
+                thumbnail={data.thumbnail}
+                author={data.author}
+              />
+            )
+          })}
+        </View>
         <View
           style={{
             marginVertical: theme.spacing.s,
